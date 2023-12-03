a908dae356b4ad9db64855a0fe6c076ff476dad2
huzejun <huzejun1990>
2023-12-04 04:01:49 +0800

    defer、recover实现异常捕获和处理2 2023年12月4日04:01:43

---
+++ defer-recover-panic/main.go
@@ -0,0 +1,12 @@
+// @Author huzejun 2023/12/4 3:11:00
+package main
+
+import _case "basis/defer-recover-panic/case"
+
+func main() {
+	//_case.DeferCase1()
+	//_case.DeferCase2()
+	//_case.DeferCase3()
+	//_case.ExceptionCase()
+	_case.FileReadCase()
+}
